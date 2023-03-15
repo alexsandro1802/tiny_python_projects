4c0af9546dc6593355172168571b64e55a3fe94d
alexsandro1802 <123095026+alexsandro1802@users.noreply.github.com>
2023-03-14 23:35:20 -0400

    Add files via upload

--- 06_wc/test.py
+++ 06_wc/test.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
-"""tests for wc.py"""
+"""tests for jump.py"""
 
 import os
-import random
-import re
-import string
 from subprocess import getstatusoutput
 
-prg = './wc.py'
-empty = './inputs/empty.txt'
-one_line = './inputs/one.txt'
-two_lines = './inputs/two.txt'
-fox = '../inputs/fox.txt'
-sonnet = '../inputs/sonnet-29.txt'
+prg = './jump.py'
 
 
 # --------------------------------------------------
@@ -29,79 +21,22 @@ def test_usage():
     for flag in ['-h', '--help']:
         rv, out = getstatusoutput(f'{prg} {flag}')
         assert rv == 0
-        assert re.match("usage", out, re.IGNORECASE)
+        assert out.lower().startswith('usage')
 
 
 # --------------------------------------------------
-def random_string():
-    """generate a random string"""
+def test_01():
+    """test"""
 
-    k = random.randint(5, 10)
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=k))
-
-
-# --------------------------------------------------
-def test_bad_file():
-    """bad_file"""
-
-    bad = random_string()
-    rv, out = getstatusoutput(f'{prg} {bad}')
-    assert rv != 0
-    assert re.search(f"No such file or directory: '{bad}'", out)
-
-
-# --------------------------------------------------
-def test_empty():
-    """Test on empty"""
-
-    rv, out = getstatusoutput(f'{prg} {empty}')
+    rv, out = getstatusoutput(f'{prg} 123-456-7890')
     assert rv == 0
-    assert out.rstrip() == '       0       0       0 ./inputs/empty.txt'
+    assert out == '987-604-3215'
 
 
 # --------------------------------------------------
-def test_one():
-    """Test on one"""
+def test_02():
+    """test"""
 
-    rv, out = getstatusoutput(f'{prg} {one_line}')
+    rv, out = getstatusoutput(f'{prg} "That number to call is 098-765-4321."')
     assert rv == 0
-    assert out.rstrip() == '       1       1       2 ./inputs/one.txt'
-
-
-# --------------------------------------------------
-def test_two():
-    """Test on two"""
-
-    rv, out = getstatusoutput(f'{prg} {two_lines}')
-    assert rv == 0
-    assert out.rstrip() == '       2       2       4 ./inputs/two.txt'
-
-
-# --------------------------------------------------
-def test_fox():
-    """Test on fox"""
-
-    rv, out = getstatusoutput(f'{prg} {fox}')
-    assert rv == 0
-    assert out.rstrip() == '       1       9      45 ../inputs/fox.txt'
-
-
-# --------------------------------------------------
-def test_more():
-    """Test on more than one file"""
-
-    rv, out = getstatusoutput(f'{prg} {fox} {sonnet}')
-    expected = ('       1       9      45 ../inputs/fox.txt\n'
-                '      17     118     661 ../inputs/sonnet-29.txt\n'
-                '      18     127     706 total')
-    assert rv == 0
-    assert out.rstrip() == expected
-
-
-# --------------------------------------------------
-def test_stdin():
-    """Test on stdin"""
-
-    rv, out = getstatusoutput(f'{prg} < {fox}')
-    assert rv == 0
-    assert out.rstrip() == '       1       9      45 <stdin>'
+    assert out.rstrip() == 'That number to call is 512-340-6789.'
